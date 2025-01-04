8319691095362a96b45c608f1269b3d33d4f76e9
alex <leksfix@gmail.com>
2025-01-04 19:49:16 +0100

    Added brain-prime game

---
+++ brain_games/prime.py
@@ -0,0 +1,19 @@
+import random
+import math
+
+
+INSTRUCTION = 'Answer "yes" if given number is prime. Otherwise answer "no".'
+
+
+def is_prime(n):
+    i = 2
+    while i <= math.sqrt(n):
+        if not n % i:
+            return False
+        i += 1
+    return True
+
+
+def game_question():
+    n = random.randrange(2, 100)
+    return (str(n), 'yes' if is_prime(n) else 'no')
